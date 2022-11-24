e5563e00cb5fafa6a6a81fdeb4aefd7bd0e6898d
whatchuThinkin <spearhead0427@gmail.com>
2022-11-24 17:20:02 +0900

    initial

---
+++ dynamicTabIndicator/dynamicTabIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamicTabIndicatorApp.swift
+//  dynamicTabIndicator
+//
+//  Created by 1100690 on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct dynamicTabIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
